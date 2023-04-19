5be679cefde255479e7d2ef9ee128ed59a1de230
Peng-xx <Yanyy_xx@163.com>
2023-04-19 18:22:46 +0800

    第三次提交

---
+++ interface-demo1/src/main/java/com/htt/demo/Demo2.java
@@ -0,0 +1,17 @@
+package com.htt.demo;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+public class Demo2 { 
+	@Test
+    public void test2(){
+        System.out.println("测试新增文件，git代码提交");
+    }
+}
